e3881da73d3101e8633f20088227e5cfb0aff217
Max Blennemann <maxblennemann@gmail.com>
2023-10-25 23:33:32 -0700

    something is worng

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 //https://discord.com/api/oauth2/authorize?client_id=1166905604909043772&permissions=8&scope=bot
 const TOKEN = require("fs").readFileSync("./token.txt").toString();
+const CLIENTID = require("fs").readFileSync("./clientid.txt").toString();
 const Discord = require("discord.js");
-const {SlashCommandBuilder} = require("@discordjs/builders");
+const {REST, Routes} = require("discord.js");
+const reddit = require("./redditAccess/snoowrapFetch.js")
+
+const commands = [
+    {
+        name: "check", description: "replies with hey"
+    },
+    {
+        name: "get post", description: "Gets a reddit post using the api",
+    }
+]
+const rest = new REST({version: "10"}).setToken(TOKEN);
+
 const client = new Discord.Client({
     intents: [
         Discord.GatewayIntentBits.Guilds,
@@ -17,21 +30,34 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", msg => {
-    const id = msg.author.id;
     if (msg.content === 'ping') {
         msg.reply('pong').then(r => console.log(r));
         msg.react('👍').then(r => console.log(r));
     } else if (msg.content === 'check') {
         client.channels.cache.get(msg.channel.id).send('your ip is 141.12.12.1');
+    } else if (msg.content.startsWith('get post')) {
+        const args = msg.content.split(' ');
+        if (args.length < 3) {
+            msg.reply('Please specify a subreddit');
+        } else {
+            const subreddit = args[2];
+            msg.reply('Getting post from subreddit ' + subreddit);
+            reddit.
+            redditAccess.snooWrapFetch.getRandomHotPost(subreddit).then(post => msg.reply(post[0] + "\n" + post[1]));
+        }
     }
-});
+    const id = msg.author.id;
+    const name = msg.author.username;
+    console.log("Message from " + name + " (" + id + "): " + msg.content);
+})
+;
 
 client.on("interactionCreate", interaction => {
-    if (!interaction.isCommand()) return;
-    const {commandName} = interaction;
-    let a = new SlashCommandBuilder('checkmate', );
+    if (!interaction.isCommand())
+        return;
+    console.log(interaction.user.username + " used command " + interaction.commandName);
 
-    if (commandName === 'check') {
+    if (interaction.commandName === 'check') {
         interaction.reply('mate!').then(r => console.log(r));
     }
 });
---
+++ redditAccess/snoowrapFetch.js
@@ -0,0 +1,17 @@
+const snoowrap = require('snoowrap');
+const REDDITID = require("fs").readFileSync("redditAccess/redditSecret.txt").toString();
+const MYPW = require("fs").readFileSync("redditAccess/redditPassword.txt").toString();
+const r = new snoowrap({
+    userAgent: 'redditBotByTheBladeBattler',
+    clientId: 'khuDsX-x4-c9ZG9TzeSlQg',
+    clientSecret: REDDITID,
+    username: 'TheBladeBattler',
+    password: MYPW
+});
+
+async function getRandomHotPost(subreddit) {
+    const submissions = await r.getSubreddit(subreddit).getHot();
+    const randomIndex = Math.floor(Math.random() * submissions.length);
+    const randomPost = submissions[randomIndex];
+    return [randomPost.title, randomPost.url];
+}
